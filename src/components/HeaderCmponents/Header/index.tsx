12a815d8fac1000b99fe239916faf96d63a97390
Павел Беляев <philipsxenium93@gmail.com>
2024-01-10 18:52:11 +0700

    header: logic logo, style component

--- src/components/HeaderCmponents/Header/index.tsx
+++ src/components/HeaderCmponents/Header/index.tsx
@@ -4,10 +4,13 @@ import { BurgerIcon } from '../../../assets/SVG/BurgerIcon';
 import { SearchIcon } from '../../../assets/SVG/SearchIcon';
 import { SearchForm } from '../SearchForm';
 import { Logo } from '../../Logo';
+import { useLocation } from 'react-router-dom';
 
 export const Header = () => {
+  const { pathname } = useLocation();
   const [isVisible, setIsVisible] = useState(true);
   const [isAble, setIsAble] = useState(false);
+  const isHome = pathname !== '/';
 
   const showInput = useCallback(() => {
     setIsVisible(!isVisible);
@@ -18,21 +21,29 @@ export const Header = () => {
   }, [isAble]);
 
   return (
-    <div className='container grid grid-cols-[0.6fr_1fr_0.6fr] py-2'>
-      <div className='flex gap-x-2 self-center'>
-        <HeaderButton className='hover:bg-[#f7f7f7]' onCLick={showSideBar}>
-          <BurgerIcon w='20' h='20' />
-        </HeaderButton>
-        <HeaderButton className='hover:bg-[#f7f7f7]' onCLick={showInput}>
-          <SearchIcon w='20' h='20' fill='transparent' />
-        </HeaderButton>
-        <SearchForm isVisible={isVisible} />
+    <div className='relative'>
+      <div
+        className={`container grid grid-cols-[0.6fr_1fr_0.6fr] py-2 ${
+          isHome
+            ? 'after:absolute after:border-[#C7C7C7] after:border-b after:bottom-0 after:w-full after:left-0'
+            : ''
+        }`}>
+        <div className='flex gap-x-2 self-center'>
+          <HeaderButton className='hover:bg-[#f7f7f7]' onCLick={showSideBar}>
+            <BurgerIcon w='20' h='20' />
+          </HeaderButton>
+          <HeaderButton className='hover:bg-[#f7f7f7]' onCLick={showInput}>
+            <SearchIcon w='20' h='20' fill='transparent' />
+          </HeaderButton>
+          <SearchForm isVisible={isVisible} />
+        </div>
+        <div className='grid place-self-center '>
+          {isHome && <Logo size='text-2xl' />}
+        </div>
+        <div className='grid justify-self-end content-center px-2'>
+          login...
+        </div>
       </div>
-      <div className='grid place-self-center '>
-        {/*<Logo size='2xl' />*/}
-        language
-      </div>
-      <div className='grid justify-self-end content-center px-2'>login...</div>
     </div>
   );
 };
