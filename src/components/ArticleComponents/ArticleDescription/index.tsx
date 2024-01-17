629fea68d144361ccabfe3bd0f4f9c83b42d57c6
Павел Беляев <philipsxenium93@gmail.com>
2024-01-17 15:27:26 +0700

    added component ArticleDescription

--- src/components/ArticleComponents/ArticleDescription/index.tsx
+++ src/components/ArticleComponents/ArticleDescription/index.tsx
@@ -1,5 +1,34 @@
-import React from 'react';
+import React, { FC } from 'react';
 
-export const ArticleDescription = () => {
-  return <div></div>;
+export const ArticleDescription: FC = () => {
+  return (
+    <div className='container flex flex-col gap-y-2.5'>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus
+        adipisci animi aperiam, architecto consequuntur cumque ducimus enim esse
+        eum exercitationem expedita, laboriosam laborum magnam modi pariatur
+        perspiciatis rerum, tempora veritatis?
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus ad
+        adipisci alias debitis dolorum eligendi ex fuga illo libero nihil nobis
+        officia praesentium rem soluta ullam, vitae voluptate voluptatibus! Ad
+        amet aperiam blanditiis consequuntur corporis distinctio dolores ea et
+        fugiat inventore iure iusto labore magni, molestiae odio, officiis
+        provident quae, qui quo quod quos repellendus repudiandae sed suscipit
+        veritatis voluptatum.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium
+        aspernatur consequatur enim, error eum facere facilis, ipsam libero
+        nobis optio, qui reiciendis. Consequuntur dolorem doloribus facere ipsa
+        nulla quis. Sunt?
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Commodi eaque
+        expedita ipsum nemo neque nostrum odio, quod reprehenderit sed
+        veritatis!
+      </p>
+    </div>
+  );
 };
--- src/pages/ArticlePage/index.tsx
+++ src/pages/ArticlePage/index.tsx
@@ -1,5 +1,6 @@
 import React, { FC } from 'react';
 import {
+  ArticleDescription,
   ArticleHeading,
   ArticlePoster,
   Byline,
@@ -21,90 +22,9 @@ export const ArticlePage: FC = () => {
         description='Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deleniti eaque illum ipsam libero, maxime natus praesentium quidem quos ratione veniam? Ab accusantium, ad at consectetur consequatur dolorum ea modi sint.'
       />
       <Byline byline='By name' date='1993-03-15T14:03:56-05:00' />
-      <div className='container flex flex-col gap-y-2.5'>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus
-          adipisci animi aperiam, architecto consequuntur cumque ducimus enim
-          esse eum exercitationem expedita, laboriosam laborum magnam modi
-          pariatur perspiciatis rerum, tempora veritatis?
-        </p>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus ad
-          adipisci alias debitis dolorum eligendi ex fuga illo libero nihil
-          nobis officia praesentium rem soluta ullam, vitae voluptate
-          voluptatibus! Ad amet aperiam blanditiis consequuntur corporis
-          distinctio dolores ea et fugiat inventore iure iusto labore magni,
-          molestiae odio, officiis provident quae, qui quo quod quos repellendus
-          repudiandae sed suscipit veritatis voluptatum.
-        </p>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium
-          aspernatur consequatur enim, error eum facere facilis, ipsam libero
-          nobis optio, qui reiciendis. Consequuntur dolorem doloribus facere
-          ipsa nulla quis. Sunt?
-        </p>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Commodi
-          eaque expedita ipsum nemo neque nostrum odio, quod reprehenderit sed
-          veritatis!
-        </p>
-      </div>
-      <div className='container flex flex-col gap-y-2.5'>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus
-          adipisci animi aperiam, architecto consequuntur cumque ducimus enim
-          esse eum exercitationem expedita, laboriosam laborum magnam modi
-          pariatur perspiciatis rerum, tempora veritatis?
-        </p>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus ad
-          adipisci alias debitis dolorum eligendi ex fuga illo libero nihil
-          nobis officia praesentium rem soluta ullam, vitae voluptate
-          voluptatibus! Ad amet aperiam blanditiis consequuntur corporis
-          distinctio dolores ea et fugiat inventore iure iusto labore magni,
-          molestiae odio, officiis provident quae, qui quo quod quos repellendus
-          repudiandae sed suscipit veritatis voluptatum.
-        </p>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium
-          aspernatur consequatur enim, error eum facere facilis, ipsam libero
-          nobis optio, qui reiciendis. Consequuntur dolorem doloribus facere
-          ipsa nulla quis. Sunt?
-        </p>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Commodi
-          eaque expedita ipsum nemo neque nostrum odio, quod reprehenderit sed
-          veritatis!
-        </p>
-      </div>
-      <div className='container flex flex-col gap-y-2.5'>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus
-          adipisci animi aperiam, architecto consequuntur cumque ducimus enim
-          esse eum exercitationem expedita, laboriosam laborum magnam modi
-          pariatur perspiciatis rerum, tempora veritatis?
-        </p>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus ad
-          adipisci alias debitis dolorum eligendi ex fuga illo libero nihil
-          nobis officia praesentium rem soluta ullam, vitae voluptate
-          voluptatibus! Ad amet aperiam blanditiis consequuntur corporis
-          distinctio dolores ea et fugiat inventore iure iusto labore magni,
-          molestiae odio, officiis provident quae, qui quo quod quos repellendus
-          repudiandae sed suscipit veritatis voluptatum.
-        </p>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium
-          aspernatur consequatur enim, error eum facere facilis, ipsam libero
-          nobis optio, qui reiciendis. Consequuntur dolorem doloribus facere
-          ipsa nulla quis. Sunt?
-        </p>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Commodi
-          eaque expedita ipsum nemo neque nostrum odio, quod reprehenderit sed
-          veritatis!
-        </p>
-      </div>
+      <ArticleDescription />
+      <ArticleDescription />
+      <ArticleDescription />
     </div>
   );
 };
