846de2838a2661ea820134a63e747dc16a9cd87f
Павел Беляев <philipsxenium93@gmail.com>
2024-02-09 12:46:20 +0700

    delete Archive because not data Api

--- src/pages/SectionPage/index.tsx
+++ src/pages/SectionPage/index.tsx
@@ -1,23 +1,18 @@
 import React, { FC, useEffect } from 'react';
 import { useLocation } from 'react-router-dom';
-import { fetchSectionsNews, sectionsSlice, useAppDispatch } from '../../store';
-import { useSelector } from 'react-redux';
+import { fetchSectionsNews, useAppDispatch } from '../../store';
 import {
   CenterColumn,
-  Description,
   LeftColumn,
   NewsGrid,
   RightColumn,
-  Title,
   Wrapper,
 } from '../../components';
-import { ArchiveSearch } from '../../components/ArchiveComponents';
 
 export const SectionPage: FC = () => {
   const { pathname } = useLocation();
   const sectionName = pathname.split(/[\d/]/g).slice(-1).join('');
   const dispatch = useAppDispatch();
-  const { topNews, dataSections } = useSelector(sectionsSlice);
 
   useEffect(() => {
     dispatch(
@@ -30,6 +25,7 @@ export const SectionPage: FC = () => {
               : sectionName,
       }),
     );
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [pathname]);
 
   return (
@@ -45,29 +41,6 @@ export const SectionPage: FC = () => {
         <RightColumn />
         <NewsGrid />
       </Wrapper.NewsBlock>
-      <div className='flex flex-col gap-y-2'>
-        <ArchiveSearch />
-        <div className='flex flex-col'>
-          <div className='flex'>
-            <div>sort</div>
-            <div>sort</div>
-          </div>
-          <span>result</span>
-        </div>
-        <div className='flex flex-col gap-y-[20px]'>
-          {[1, 2, 3].map(() => (
-            <div className='grid grid-cols-[0.5fr_3fr_1fr] border-b border-[#C7C7C7] last:border-none pb-[20px]'>
-              <div>date</div>
-              <div>
-                <Title title={''} size={'S'} />
-                <Description text={''} />
-                <span>byline</span>
-              </div>
-              <div>foto</div>
-            </div>
-          ))}
-        </div>
-      </div>
     </>
   );
 };
